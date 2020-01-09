416b2823576aa18cc552532920ae21a1a1a1be80
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-09 20:05:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,11 +18,11 @@ def literal_aoh
     {:name => "Joelle van Dyne", :occupation => "Radio Personality"}
     {:name => "Pat Monteseian", :occupation => "Staff"}
     {:name => "Kate Gompert", :occupation => "None"}
-    
+    {:name => "Bruce Green", :occupation => "Fan of Mildred" }
     ]
+end
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-end
 
 def aoh_lookup(aoh, row, key)
 end
